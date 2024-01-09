044b700678ff9e21638a4a0c46d73bcaa4de9ae5
lymslive <403708621@qq.com>
2024-01-10 00:30:06 +0800

    fix dup id

--- src/stores.js
+++ src/stores.js
@@ -7,7 +7,7 @@ export const alert = writable('欢迎使用待办事项备忘录小工具')
 const initialTodos = [
   { id: 1, name: '吃饭', completed: true },
   { id: 2, name: '睡觉', completed: false },
-  { id: 2, name: '打豆豆', completed: false },
+  { id: 3, name: '打豆豆', completed: false },
 ]
 
 export const todos = localStore('lymslive-wetodo', initialTodos)
